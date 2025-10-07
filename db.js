404ce1bcb965eba7f22220c184aaccbe7160c06a
inglbv01-max <inglbv01@gmail.com>
2025-10-06 21:25:04 -0600

    Update db.js

--- db.js
+++ db.js
@@ -8,28 +8,14 @@ function initDB() {
 
     request.onupgradeneeded = (e) => {
       db = e.target.result;
-
-      if(!db.objectStoreNames.contains('viajes')) 
-        db.createObjectStore('viajes', { keyPath: 'id', autoIncrement: true });
-
-      if(!db.objectStoreNames.contains('camiones')) 
-        db.createObjectStore('camiones', { keyPath: 'id', autoIncrement: true });
-
-      if(!db.objectStoreNames.contains('ubicaciones')) 
-        db.createObjectStore('ubicaciones', { keyPath: 'id', autoIncrement: true });
-
-      if(!db.objectStoreNames.contains('lotes')) 
-        db.createObjectStore('lotes', { keyPath: 'id', autoIncrement: true });
-
-      // CORRECCIÓN: store de usuarios con keyPath 'username'
-      if(!db.objectStoreNames.contains('usuarios')) 
-        db.createObjectStore('usuarios', { keyPath: 'username' });
+      if(!db.objectStoreNames.contains('viajes')) db.createObjectStore('viajes', { keyPath: 'id', autoIncrement: true });
+      if(!db.objectStoreNames.contains('camiones')) db.createObjectStore('camiones', { keyPath: 'id', autoIncrement: true });
+      if(!db.objectStoreNames.contains('ubicaciones')) db.createObjectStore('ubicaciones', { keyPath: 'id', autoIncrement: true });
+      if(!db.objectStoreNames.contains('lotes')) db.createObjectStore('lotes', { keyPath: 'id', autoIncrement: true });
+      if(!db.objectStoreNames.contains('usuarios')) db.createObjectStore('usuarios', { keyPath: 'id', autoIncrement: true });
     };
 
-    request.onsuccess = (e) => { 
-      db = e.target.result; 
-      resolve(); 
-    };
+    request.onsuccess = (e) => { db = e.target.result; resolve(); };
     request.onerror = (e) => reject(e);
   });
 }
@@ -74,6 +60,3 @@ function deleteItem(storeName, id) {
     req.onerror = () => reject(req.error);
   });
 }
-
-// Inicializar DB al cargar
-initDB().then(() => console.log("IndexedDB inicializada"));
